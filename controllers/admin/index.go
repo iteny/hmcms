8e0b6088e9823b6674506cd8b83ac1a71d50825f
赵钟品 <iteny@msn.com>
2017-05-28 21:29:49 +0800

    no message

--- controllers/admin/index.go
+++ controllers/admin/index.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"fmt"
 	"hmcms/models/sqlite"
+	"strconv"
 )
 
 type IndexController struct {
@@ -13,12 +14,34 @@ func (c *IndexController) Index() {
 	rows, _ := sqlite.AuthRuleDb.GetOneMenu()
 	fmt.Println(rows, 111)
 	c.Data["menu"] = rows
-	c.VerifyLogin()
+	// c.VerifyLogin()
 	c.Data["username"] = c.GetSession("username")
 	c.TplName = "admin/index.html"
 }
 
-//获取顶部菜单
-// func (c *IndexController) GetOneMenu() {
-//
-// }
+//获取左侧菜单
+func (c *IndexController) GetLeftMenu() {
+	// pid := c.GetString("pid")
+	pid := c.Input().Get("pid")
+	intpid, _ := strconv.Atoi(pid)
+	rows, _ := sqlite.AuthRuleDb.GetTwoMenu(intpid)
+
+	for k, v := range rows {
+
+		throws, _ := sqlite.AuthRuleDb.GetTwoMenu(v.Id)
+		for tk, _ := range throws {
+			rows[k].Children = append(rows[k].Children, throws[tk])
+		}
+
+		// for _, d := range throws {
+		// 	node := sqlite.AuthRule{Id: v.Id, Title: v.Title, Children: sqlite.AuthRule{Id: d.Id, Title: d.Title}}
+		// 	// sqlite.AuthRule.Children = append(sqlite.AuthRule.Children, &node)
+		// }
+
+	}
+	// fmt.Println(intpid)
+	fmt.Println(rows)
+	c.Data["json"] = rows
+	c.ServeJSON()
+	return
+}
--- models/sqlite/authrule.go
+++ models/sqlite/authrule.go
@@ -8,16 +8,17 @@ func init() {
 }
 
 type AuthRule struct {
-	Id        int    `json:"id" xorm:"integer(11) notnull unique pk autoincr"`
-	Name      string `json:"name" xorm:"text(80) notnull default ''"`
-	Title     string `json:"title" xorm:"text(20) notnull default ''"`
-	Type      int    `json:"type" xorm:"integer(1) notnull default 0 index"`
-	Status    int    `json:"status" xorm:"integer(1) notnull default 0 index"`
-	Condition string `json:"condition" xorm:"text(100) notnull default ''"`
-	Pid       int    `json:"pid" xorm:"integer(6) notnull default 0 index"`
-	Isshow    int    `json:"isshow" xorm:"integer(1) notnull default 0 index"`
-	Sort      int    `json:"sort" xorm:"integer(6) notnull default 0 index"`
-	Icon      string `json:"icon" xorm:"text(50) notnull default ''"`
+	Id        int         `json:"id" xorm:"integer(11) notnull unique pk autoincr"`
+	Name      string      `json:"name" xorm:"text(80) notnull default ''"`
+	Title     string      `json:"title" xorm:"text(20) notnull default ''"`
+	Type      int         `json:"type" xorm:"integer(1) notnull default 0 index"`
+	Status    int         `json:"status" xorm:"integer(1) notnull default 0 index"`
+	Condition string      `json:"condition" xorm:"text(100) notnull default ''"`
+	Pid       int         `json:"pid" xorm:"integer(6) notnull default 0 index"`
+	Isshow    int         `json:"isshow" xorm:"integer(1) notnull default 0 index"`
+	Sort      int         `json:"sort" xorm:"integer(6) notnull default 0 index"`
+	Icon      string      `json:"icon" xorm:"text(50) notnull default ''"`
+	Children  []*AuthRule `json:"children"`
 }
 
 func NewAuthRule() *AuthRule {
@@ -27,5 +28,11 @@ func NewAuthRule() *AuthRule {
 //获取一级菜单
 func (a *AuthRule) GetOneMenu() (ar []AuthRule, err error) {
 	err = x.Where("pid = ? AND isshow = ?", 0, 1).Find(&ar)
+	return ar, err
+}
+
+//获取二级菜单
+func (a *AuthRule) GetTwoMenu(pid int) (ar []*AuthRule, err error) {
+	err = x.Asc("sort").Cols("id,title,icon").Where("pid= ? AND isshow= ?", pid, 1).Find(&ar)
 	return ar, err
 }
